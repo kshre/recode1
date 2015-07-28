027639af8c3e022a90663eb636edb8a5dd03b339
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-07-28 23:00:54 +0530

    adduser POST router added; spruced up homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'everything is plumbing :P' });
 });
 
 /* GET userlist page */
@@ -18,6 +18,20 @@ router.get('/userlist', function(req, res, next) {
 /* GET newuser page*/
 router.get('/newuser', function(req, res, next) {
   res.render('newuser', {title: "Add new user"});
+});
+
+/* POST adduser */
+router.post('/adduser', function(req, res, next) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  
+  var username = req.body.username;
+  var email = req.body.useremail;
+
+  collection.insert( { "username": username, "email": email} , function(err, docs) {
+    if(err) {res.send("there was a problem adding data to the database"); }
+    else { res.redirect("userlist"); }
+  });
 });
 
 module.exports = router;
