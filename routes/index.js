a0d035ebe40fa64ce42c10a3c08298d9e63d5854
Saurabh Kulshreshtha <saurkulsh@gmail.com>
2015-08-06 22:53:30 +0530

    POST adduser implemented

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,23 @@ router.get('/newuser', function(req, res, next) {
   res.render('newuser', { title: "Add new user"});
 });
 
+/* POST adduser */
+router.post('/adduser', function(req, res, next) {
+  // database setup
+  var db = req.db;
+  var collection = db.get('usercollection');
+  
+  // get username and useremail from the POST query
+  var username = req.body.username;
+  var useremail = req.body.useremail;
+
+  // insert into db
+  collection.insert( { 'username': username, 'email': useremail }, function(e, d) {
+    if(e){
+      res.send("There was a problem storing data into the database");
+    } else {
+      res.redirect('userlist');
+    }
+  });  
+});
 module.exports = router;
